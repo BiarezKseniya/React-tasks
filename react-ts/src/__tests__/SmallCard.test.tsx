895ce83882fc27443ee426dfede6d977502e7433
Kseniya Biarezina <ksenia-2010@tut.by>
2023-11-12 20:08:36 +0100

    feat: add Small card tests

---
+++ react-ts/src/__tests__/SmallCard.test.tsx
@@ -0,0 +1,38 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import SmallCard from '../components/small-card/SmallCard';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AppProvider } from '../components/context/AppState';
+
+test('renders the relevant card data', () => {
+  render(
+    <Router>
+      <AppProvider>
+        <SmallCard id={1} name="Sample Name" description="Sample Description" />
+      </AppProvider>
+    </Router>
+  );
+  const name = screen.getByText('Sample Name');
+  const description = screen.getByText('Sample Description');
+
+  expect(name).toBeDefined();
+  expect(description).toBeDefined();
+});
+
+test('opens detailed card modal', async () => {
+  const { getByText } = render(
+    <Router>
+      <AppProvider>
+        <SmallCard id={1} name="Test" description="Test description" />
+      </AppProvider>
+    </Router>
+  );
+
+  fireEvent.click(getByText('Test'));
+  expect(window.location.pathname + window.location.search).toBe(
+    '/modal?front-page=1&pokemon=1'
+  );
+});
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
