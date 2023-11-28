c1ca34e87a605ae7e93f9e5cf1884f9f8b92417e
Kseniya Biarezina <ksenia-2010@tut.by>
2023-11-28 17:17:42 +0100

    feat: set up router

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -2,9 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import ControlledForm from './components/ControlledForm';
+import UncontrolledForm from './components/UncontrolledForm';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'controlled-form',
+        element: <ControlledForm />,
+      },
+      {
+        path: 'uncontrolled-form',
+        element: <UncontrolledForm />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
